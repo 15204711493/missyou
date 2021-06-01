e67555a68881bdb6b27a323caa83fe80f1b6e71b
袁建强 <15204711493@163.com>
2021-06-01 22:18:42 +0800

    token部分测试

--- src/main/java/com/lin/missyou/until/JwtToken.java
+++ src/main/java/com/lin/missyou/until/JwtToken.java
@@ -1,14 +1,15 @@
 package com.lin.missyou.until;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class JwtToken {
@@ -18,7 +19,7 @@ public class JwtToken {
     private static Integer defaultScope = 8;
 
     @Value("${missyou.security.jwt-key}")
-    public  void setJwtKey(String jwtKey) {
+    public void setJwtKey(String jwtKey) {
         JwtToken.jwtKey = jwtKey;
     }
 
@@ -27,26 +28,40 @@ public class JwtToken {
         JwtToken.expiredTimeIn = expiredTimeIn;
     }
 
-    public static String makeToken(Long uid){
-        return JwtToken.getToken(uid,JwtToken.defaultScope);
+    public static String makeToken(Long uid) {
+        return JwtToken.getToken(uid, JwtToken.defaultScope);
     }
 
 
-
-
-    public static String makeToken(Long uid, Integer scope){
-        return JwtToken.getToken(uid,scope);
+    public static String makeToken(Long uid, Integer scope) {
+        return JwtToken.getToken(uid, scope);
 
     }
 
-    public static String getToken(Long uid,Integer scope){
+    public Optional<Map<String, Claim>> getClaims(String token) {
+        DecodedJWT decodedJWT;
+        Algorithm algorithm = Algorithm.HMAC256(JwtToken.jwtKey);
+        JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+        try {
+            decodedJWT = jwtVerifier.verify(token);
+        }catch (JWTVerificationException e){
+            return Optional.empty();
+        }
+        Map<String, Claim> claims = decodedJWT.getClaims();
+        return Optional.of(claims);
+
+
+    }
+
+
+    public static String getToken(Long uid, Integer scope) {
 
         Algorithm algorithm = Algorithm.HMAC256(JwtToken.jwtKey);
-        Map<String,Date> dateMap =  JwtToken.calculateExpiredIssues();
+        Map<String, Date> dateMap = JwtToken.calculateExpiredIssues();
 
         String jwt = JWT.create()
-                .withClaim("uid",uid)
-                .withClaim("scope",scope)
+                .withClaim("uid", uid)
+                .withClaim("scope", scope)
                 .withExpiresAt(dateMap.get("expiredTime"))
                 .withIssuedAt(dateMap.get("now"))
                 .sign(algorithm);
@@ -58,7 +73,7 @@ public class JwtToken {
         Map<String, Date> map = new HashMap<>();
         Calendar calendar = Calendar.getInstance();
         Date now = calendar.getTime();
-        calendar.add(Calendar.SECOND,JwtToken.expiredTimeIn);
+        calendar.add(Calendar.SECOND, JwtToken.expiredTimeIn);
         map.put("now", now);
         map.put("expiredTime", calendar.getTime());
         return map;
